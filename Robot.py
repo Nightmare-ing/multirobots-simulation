5a79d48ed13397e172d90098a2f16a043eab166a
Qingsong Hu <kingsanhu@gmail.com>
2023-12-23 21:41:26 +0800

    Run a demo of two robots moving along x and y axis

--- Robot.py
+++ Robot.py
@@ -3,10 +3,12 @@ import numpy as np
 
 
 class Camera:
-    def __init__(self, field_angle=np.pi/2, visible_radius=2.0):
+    def __init__(self, field_angle=np.pi / 2, visible_radius=2.0, visible_range=np.array([0.25, 2.0]),
+                 rotate_speed=0.1):
         self.field_angle = field_angle
         self.visible_radius = visible_radius
-        self.visible_range = np.array([0.25, 2.0])
+        self.visible_range = visible_range
+        self.rotate_speed = rotate_speed
 
 
 class Robot:
@@ -20,37 +22,36 @@ class Robot:
         self.posture = np.array(initial_posture)
         self.camera = camera
         self.camera_angle = camera_angle
+        self.color = np.random.uniform(0.3, 1, 3)
 
     def update_position(self, new_posture):
         self.posture = np.array(new_posture)
 
-    def draw(self):
+    def draw(self, fig):
         # generate random colors for each robot, the color should not be too light to see
-        color = np.random.uniform(0.3, 1, 3)
 
         # draw robot point
-        plt.plot(self.posture[0], self.posture[1], 'o', color=color, markersize=8,
+        fig.plot(self.posture[0], self.posture[1], 'o', color=self.color, markersize=8,
                  label=f'Robot {self.robot_id}')
 
         # draw sector part of fov
         fov_radius = self.camera.visible_radius
-        fov_theta = np.linspace(self.posture[2] + self.camera_angle - self.camera.field_angle/2,
-                                self.posture[2] + self.camera_angle + self.camera.field_angle/2,
+        fov_theta = np.linspace(self.posture[2] + self.camera_angle - self.camera.field_angle / 2,
+                                self.posture[2] + self.camera_angle + self.camera.field_angle / 2,
                                 100)
         fov_x_start = self.posture[0] + fov_radius * self.camera.visible_range[0] * np.cos(fov_theta)
         fov_y_start = self.posture[1] + fov_radius * self.camera.visible_range[0] * np.sin(fov_theta)
         fov_x_end = self.posture[0] + fov_radius * self.camera.visible_range[1] * np.cos(fov_theta)
         fov_y_end = self.posture[1] + fov_radius * self.camera.visible_range[1] * np.sin(fov_theta)
         # add shadow of fov, alpha=0.1 means 10% transparent
-        plt.fill_between(fov_x_end, fov_y_end, color=color, alpha=0.3)
+        fig.fill_between(fov_x_end, fov_y_end, color=self.color, alpha=0.3)
         # draw fov edge
-        plt.plot(fov_x_end, fov_y_end, color=color, linestyle='--')
-        plt.fill([fov_x_start[0], fov_x_end[0], fov_x_end[-1], fov_x_start[-1]],
+        fig.plot(fov_x_end, fov_y_end, color=self.color, linestyle='--')
+        fig.fill([fov_x_start[0], fov_x_end[0], fov_x_end[-1], fov_x_start[-1]],
                  [fov_y_start[0], fov_y_end[0], fov_y_end[-1], fov_y_start[-1]],
-                 color=color, alpha=0.3)
-        plt.fill_between(fov_x_start, fov_y_start, color=plt.gcf().get_facecolor())
-        plt.plot(fov_x_start, fov_y_start, color=color, linestyle='--')
+                 color=self.color, alpha=0.3)
+        fig.fill_between(fov_x_start, fov_y_start, color=plt.gcf().get_facecolor())
+        fig.plot(fov_x_start, fov_y_start, color=self.color, linestyle='--')
 
     def __str__(self):
         return f"Robot {self.robot_id}- Position: {self.posture}"
-
--- main.py
+++ main.py
@@ -5,12 +5,24 @@ import Robot
 
 def main():
     robot = Robot.Robot()
-    robot.draw()
+    robot1 = Robot.Robot()
+    fig, ax = plt.subplots()
+    ax.set_xlim(-20, 20)
+    ax.set_ylim(-20, 20)
+    ax.autoscale(enable=False)
+    # draw the animation of robot moving in x direction with velocity 1 with fixed canvas
+    for i in range(100):
+        robot.update_position((i / 10.0, 0, 0))
+        robot1.update_position((0, i / 10.0, 0))
+        robot.draw(ax)
+        robot1.draw(ax)
+        plt.pause(0.1)
+        plt.cla()
 
 
 if __name__ == "__main__":
-    plt.xlim([-5, 5])
-    plt.ylim([-5, 5])
     main()
     plt.show()
+
+
 
