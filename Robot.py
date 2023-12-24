f3a0850d8fa58fb70188d1883392768bb430dd55
Qingsong Hu <kingsanhu@gmail.com>
2023-12-24 11:58:16 +0800

    Modify the method to draw visible field
    The original method has problem, some redundant part will be draw,
    because fill_between fills between two curves, if the second curve is
    not specified, it's set to 0

--- Robot.py
+++ Robot.py
@@ -1,4 +1,7 @@
+import math
+
 import matplotlib.pyplot as plt
+import matplotlib.patches as patches
 import numpy as np
 
 
@@ -15,7 +18,7 @@ class Robot:
     # to count how many robots have been created
     robot_num = 0
 
-    def __init__(self, initial_posture=(0, 0, 0), speed=(0, 0), camera=Camera(), camera_angle=0.0):
+    def __init__(self, initial_posture=(0, 0, 0), speed=(0, 0, 0), camera=Camera(), camera_angle=0.0):
         # give each robot a unique id for plt legend
         self.robot_id = Robot.robot_num
         Robot.robot_num += 1
@@ -25,8 +28,8 @@ class Robot:
         self.camera_angle = camera_angle
         self.color = np.random.uniform(0.3, 1, 3)
 
-    def update(self, dt, speed=(0, 0)):
-        self.posture = self.posture + self.speed * dt
+    def update(self, dt, speed=(0, 0, 0)):
+        self.posture = self.posture + (self.speed * dt)
         self.speed = np.array(speed)
 
     def draw(self, fig):
@@ -36,6 +39,25 @@ class Robot:
         fig.plot(self.posture[0], self.posture[1], 'o', color=self.color, markersize=8,
                  label=f'Robot {self.robot_id}')
 
+        fig.add_patch(
+            patches.Wedge(
+                (float(self.posture[0]), float(self.posture[1])),
+                float(self.camera.visible_radius * self.camera.visible_range[1]),
+                math.degrees(self.posture[2] + self.camera_angle - self.camera.field_angle / 2),
+                math.degrees(self.posture[2] + self.camera_angle + self.camera.field_angle / 2),
+                color=self.color, alpha=0.3
+            )
+        )
+
+        fig.add_patch(
+            patches.Wedge(
+                (float(self.posture[0]), float(self.posture[1])),
+                float(self.camera.visible_radius * self.camera.visible_range[0]),
+                math.degrees(self.posture[2] + self.camera_angle - self.camera.field_angle / 2),
+                math.degrees(self.posture[2] + self.camera_angle + self.camera.field_angle / 2),
+                color=plt.gcf().get_facecolor()
+            )
+        )
         # draw sector part of fov
         fov_radius = self.camera.visible_radius
         fov_theta = np.linspace(self.posture[2] + self.camera_angle - self.camera.field_angle / 2,
@@ -45,14 +67,8 @@ class Robot:
         fov_y_start = self.posture[1] + fov_radius * self.camera.visible_range[0] * np.sin(fov_theta)
         fov_x_end = self.posture[0] + fov_radius * self.camera.visible_range[1] * np.cos(fov_theta)
         fov_y_end = self.posture[1] + fov_radius * self.camera.visible_range[1] * np.sin(fov_theta)
-        # add shadow of fov, alpha=0.1 means 10% transparent
-        fig.fill_between(fov_x_end, fov_y_end, color=self.color, alpha=0.3)
         # draw fov edge
         fig.plot(fov_x_end, fov_y_end, color=self.color, linestyle='--')
-        fig.fill([fov_x_start[0], fov_x_end[0], fov_x_end[-1], fov_x_start[-1]],
-                 [fov_y_start[0], fov_y_end[0], fov_y_end[-1], fov_y_start[-1]],
-                 color=self.color, alpha=0.3)
-        fig.fill_between(fov_x_start, fov_y_start, color=plt.gcf().get_facecolor())
         fig.plot(fov_x_start, fov_y_start, color=self.color, linestyle='--')
 
     def __str__(self):
