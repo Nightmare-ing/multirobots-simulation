45d47a530569027fe0760f0ade4ed526de5f485a
Qingsong Hu <kingsanhu@gmail.com>
2023-12-24 22:43:31 +0800

    Add desired trace to the figure

--- Simulation.py
+++ Simulation.py
@@ -8,20 +8,24 @@ class Simulation:
         self.ax = ax
         self.robots = robots
         self.controller = controller
+        self.ani = None
 
     def initialize_scene(self):
         self.ax.set_xlim(-10, 10)
         self.ax.set_ylim(-10, 10)
         self.ax.set_aspect('equal')
 
+        # plot desired trace
+        self.ax.add_patch(self.controller.desired_trace)
+
         # plot robot and visible region
         for robot in self.robots:
-            self.ax.add_patch(robot.robot_point)
             for region in robot.visible_region:
                 self.ax.add_patch(region)
+            self.ax.add_patch(robot.robot_point)
 
-        return [robot.robot_point for robot in self.robots] + \
-            [region for robot in self.robots for region in robot.visible_region]
+        return [region for robot in self.robots for region in robot.visible_region] + \
+            [robot.robot_point for robot in self.robots]
 
     def update_scene(self, data):
         # update robot position with controller output
@@ -29,12 +33,14 @@ class Simulation:
         for i in range(len(self.robots)):
             self.robots[i].update(dt, speeds[i])
 
-        return [robot.robot_point for robot in self.robots] + \
-            [region for robot in self.robots for region in robot.visible_region]
+        return [region for robot in self.robots for region in robot.visible_region] + \
+            [robot.robot_point for robot in self.robots]
 
     def show_animation(self):
-        ani = animation.FuncAnimation(self.fig, self.update_scene, self.controller.speed_gen,
-                                      init_func=self.initialize_scene, interval=0.01, blit=True)
+        self.ani = animation.FuncAnimation(self.fig, self.update_scene, self.controller.speed_gen,
+                                           init_func=self.initialize_scene, interval=1, blit=True,
+                                           cache_frame_data=False, save_count=100)
         plt.show()
 
-
+    def save_animation(self, filename, fps=60):
+        self.ani.save(filename, fps=fps, dpi=200)
