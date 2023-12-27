03f335983c5430b00b71f3856005daa1e7455daa
Smile <smile@Magic.local>
2023-12-27 16:14:46 +0800

    Modify tests for speed_round
    numpy.all() will test whether all elements along a given axis evaluate
    to True

--- controller_func_test.py
+++ controller_func_test.py
@@ -9,7 +9,7 @@ class MyTestCase(unittest.TestCase):
         controller = Controller()
         rounded_speed = controller.speed_round(np.array([12, 16]))
         self.assertTrue(np.allclose(rounded_speed, np.array([6, 8])))
-        self.assertEqual(controller.speed_round(np.array([1, -1])).all(), np.array([1, -1]).all())
+        self.assertTrue(np.allclose(controller.speed_round(np.array([1, -1])), np.array([1, -1])))
 
 
 if __name__ == '__main__':
