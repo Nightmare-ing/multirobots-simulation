40aeeeac529398a9db7dc9a8a54742c0e3222f77
Smile <smile@Magic.local>
2023-12-27 16:07:48 +0800

    Add super class Controller, add round methods
    - All controller methods inheritate from the super class
    - Round methods including round speed, angular velocity

--- Controller.py
+++ Controller.py
@@ -3,7 +3,52 @@ import numpy as np
 import itertools
 
 
-class CentralController:
+class Controller:
+    __gravity_velocity = 9.81
+
+    def __init__(self):
+        self.radius = 5.0
+        self.central_point = (0.0, 0.0)
+        self.w_r = 1.0
+
+    @property
+    def _speed_range(self):
+        return np.array([0.0, 2 * self.w_r * self.radius])
+
+    @property
+    def _angular_vel_range(self):
+        return np.array([-10 * self.w_r, 10 * self.w_r])
+
+    @property
+    def _acceleration_range(self):
+        return np.array([-self.__gravity_velocity, self.__gravity_velocity])
+
+    def speed_round(self, speeds_xy):
+        """
+        round transferred in speeds to valid range
+        :param speeds_xy: speeds to be round
+        :return: rounded speeds
+        """
+        if np.linalg.norm(speeds_xy) < self._speed_range[1]:
+            return speeds_xy
+        else:
+            return speeds_xy / np.linalg.norm(speeds_xy) * self._speed_range[1]
+
+    def angular_vel_round(self, angular_vel):
+        """
+        round transferred in angular velocity to valid range
+        :param angular_vel: angular velocity to be round
+        :return: rounded angular velocity
+        """
+        if angular_vel > self._angular_vel_range[1]:
+            return self._angular_vel_range[1]
+        elif angular_vel < self._angular_vel_range[0]:
+            return self._angular_vel_range[0]
+        else:
+            return angular_vel
+
+
+class CentralController(Controller):
     def update_pos_and_draw(self, fig, data):
         for robot in self.robots:
             dt, speed = next(data)
@@ -11,10 +56,8 @@ class CentralController:
             robot.draw(fig)
 
     def __init__(self, robots):
-        self.central_point = (0, 0)
-        self.radius = 5.0
+        super().__init__()
         self.robots = robots
-        self.rotate_speed = 1.0
 
     @property
     def desired_trace(self):
@@ -27,8 +70,9 @@ class CentralController:
             for robot in self.robots:
                 cur_angle = np.arctan2(robot.posture[1] - self.central_point[1],
                                        robot.posture[0] - self.central_point[0])
-                speed = (-self.rotate_speed * self.radius * np.sin(cur_angle),
-                         self.rotate_speed * self.radius * np.cos(cur_angle),
+                speed = (-self.w_r * self.radius * np.sin(cur_angle),
+                         self.w_r * self.radius * np.cos(cur_angle),
                          0.0)
                 speeds.append(speed)
             yield dt, speeds
+
