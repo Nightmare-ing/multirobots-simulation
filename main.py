7b5a739d0866efcf0ee9d47095cfb402169eebbb
Qingsong Hu <kingsanhu@gmail.com>
2023-12-28 16:43:05 +0800

    Test DecentralizedController

--- main.py
+++ main.py
@@ -1,16 +1,19 @@
+import math
+
 import matplotlib.pyplot as plt
 import matplotlib.patches as patches
 from Parts import Robot
-from Controller import CentralController
+from Controller import CentralController, DecentralizedController
 from Simulation import Simulation
 
 
 def main():
     robots = Robot.initialize_robots(4,
-                                     posture=[(5, 0, 0), (0, 5, 0), (-5, 0, 0), (0, -5, 0)],
+                                     posture=[(5, 0, math.radians(135)), (0, 5, math.radians(225)),
+                                              (-5, 0, math.radians(-45)), (0, -5, math.radians(45))],
                                      speed=[(0, 0, 0), (0, 0, 0), (0, 0, 0), (0, 0, 0)])
     fig, ax = plt.subplots()
-    controller = CentralController(robots)
+    controller = DecentralizedController(robots)
     simulation_scene = Simulation(fig, ax, robots, controller)
     simulation_scene.show_animation()
     simulation_scene.save_animation("test.mp4")
